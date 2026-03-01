da7471a78bab186f0bd9e557b3014589ba7e633b
JIG ERA <jig111era@gmail.com>
2026-03-01 23:48:06 +0530

    ..

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -5,13 +5,12 @@ const router = express.Router()
 import { createBook, getBooks, createBooks, deleteBooks, search } from '../controllers/book.controller.js'
 import { apiKeyAuth } from '../middlewares/apiKeyAuth.js'
 
-// protected routes
-router.use(apiKeyAuth)
-
-router.post('/createBook', createBook)
-router.post('/createBooks', createBooks)
 router.get('/search', search)
 router.get('/getBooks', getBooks)
-router.delete('/deleteBooks', deleteBooks)
+
+// protected routes
+router.post('/createBook', apiKeyAuth, createBook)
+router.post('/createBooks', apiKeyAuth, createBooks)
+router.delete('/deleteBooks', apiKeyAuth, deleteBooks)
 
 export default router
